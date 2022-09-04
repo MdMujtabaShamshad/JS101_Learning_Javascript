93c093126578c8889cfb67b85d02ed8d72a0ed46
Md Mujtaba Shamshad <mdmujtabashamshad@hotmail.com>
2022-09-05 00:16:57 +0530

    my snd code

---
+++ math.js
@@ -0,0 +1,10 @@
+let one = 1; two =2; three= 3; four = 4; five = 5;
+console.log(one * two * three * four * five);
+
+
+let one= 1; two =2; three=3; four =4; five =5;
+console.log(one * 2 + two *2 + three * 2 + four * 2 + five *2);
+
+
+let one= 1; two =2; three=3; four =4; five =5; six= 6;
+console.log(one+two*2+three*3+four*3+five*2+six*2);
